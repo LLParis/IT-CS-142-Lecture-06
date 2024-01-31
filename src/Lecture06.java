8ff39a2aeaf014ae380b32f4cf9279cdc4135e6f
Your Name <you@example.com>
2024-01-31 04:41:52 -0800

    Initial Commit

---
+++ src/Lecture06.java
@@ -0,0 +1,46 @@
+public class Lecture06 {
+
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Hello, world!");
+    // SELF-CHECK PROBLEMS:
+    // Note: you will need to put some of your answers inside a comments
+
+    // Put your answer for #5A here:
+
+    // NOTE: FIRST try to predict what the output will be WITHOUT actually running the program
+    // write down your answer here
+
+    // NEXT run the program and see the results (you can copy-and-paste it here)
+    // (I'm not including it in the starter project so that people don't just run
+    //  it as the first thing they do :)  )
+
+    // LASTLY compare the two results and if they're different go back and figure out
+    // why the computer's output is different from what you predicted.
+
+    // Put your answer for #5B here:
+    // (Do this like #5A - predict, write your prediction, run the code, then explain any differences)
+
+    // Put your answer for #5C here:
+
+    // Put your answer for #6 here:
+
+    // Put your answer for #7 here:
+    // Instead of correcting the code write a brief explanation of how to fix it
+    // and include only the lines that you'll change
+
+    // Put your answer for #9 here:
+
+    // Put your answer for #12 here:
+
+    // Put your answer for #16 here:
+
+    // EXERCISES:
+
+    // Put your answer for #1 here:
+    // You will need to add the method in above main(), but then call it here
+
+    // Put your answer for #12 here:
+
+  }
+}
